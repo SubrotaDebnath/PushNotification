fb55b476f55bf3068ede3a9972ab58721f3ebeb2
SubrotaDebnath <subrota.shuvro@gmail.com>
2021-02-14 01:45:31 +0600

    Large message and inbox message style

--- app/src/main/java/subrota/shuvro/pushnotification/MainActivity.java
+++ app/src/main/java/subrota/shuvro/pushnotification/MainActivity.java
@@ -7,12 +7,13 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText titleET;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         firstSendBTN = findViewById(R.id.sendThroughFirstChannel);
         secondSendBTN = findViewById(R.id.sendThroughSecondChannel);
 
-        notificationManagerCompat =  NotificationManagerCompat.from(this);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
 
         //Creating intent for onclick event on notifications
         Intent activityIntent = new Intent(this, MainActivity.class);
@@ -39,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         //making Broadcast intent
         Intent broadcastIntent = new Intent(this, NotificationsReceiver.class);
+        broadcastIntent.putExtra("getMessage", messageET.getText().toString().trim());
         //making action intent for on click action event
         PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         //here flag (4th arg of above line)
@@ -47,13 +49,32 @@ public class MainActivity extends AppCompatActivity {
         firstSendBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (titleET.getText().toString() != null && !titleET.getText().toString().equals("")){
-                    if (messageET.getText().toString() != null && !messageET.getText().toString().equals("")){
+                if (titleET.getText().toString() != null && !titleET.getText().toString().equals("")) {
+                    if (messageET.getText().toString() != null && !messageET.getText().toString().equals("")) {
+
+                        //Here I using this method inside onclick for get message properly
+
+                        //making Broadcast intent
+                        Intent broadcastIntent = new Intent(v.getContext(), NotificationsReceiver.class);
+                        broadcastIntent.putExtra("getMessage", messageET.getText().toString().trim());
+                        //making action intent for on click action event
+                        PendingIntent actionIntent = PendingIntent.getBroadcast(v.getContext(), 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        //here flag (4th arg of above line)
+
+                        //convert drawable image into bitmap image
+                        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.profileimg);
 
                         Notification notification = new NotificationCompat.Builder(getApplicationContext(), Channels.FIRST_CHANNEL_ID)
                                 .setSmallIcon(R.drawable.ic_notifications)
                                 .setContentTitle(titleET.getText())
                                 .setContentText(messageET.getText())
+                                .setLargeIcon(largeIcon)
+                                //setting notification preview style
+                                .setStyle(new NotificationCompat.BigTextStyle()
+                                        .bigText(getString(R.string.description_text))
+                                        .setBigContentTitle("Description")
+                                        .setSummaryText("Push Notification demo")
+                                )
                                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                                 .setColor(Color.GREEN)
@@ -67,10 +88,10 @@ public class MainActivity extends AppCompatActivity {
 
                         notificationManagerCompat.notify(1, notification);
 
-                    }else {
+                    } else {
                         messageET.setError("Message is Empty.");
                     }
-                }else {
+                } else {
                     titleET.setError("Title is Empty.");
                 }
             }
@@ -80,21 +101,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (titleET.getText().toString() != null && !titleET.getText().toString().equals("")){
-                    if (messageET.getText().toString() != null && !messageET.getText().toString().equals("")){
+                if (titleET.getText().toString() != null && !titleET.getText().toString().equals("")) {
+                    if (messageET.getText().toString() != null && !messageET.getText().toString().equals("")) {
 
                         Notification notification = new NotificationCompat.Builder(getApplicationContext(), Channels.FIRST_CHANNEL_ID)
                                 .setSmallIcon(R.drawable.ic_message)
                                 .setContentTitle(titleET.getText())
                                 .setContentText(messageET.getText())
+
+                                //Can show maximum 7th position massage in ascending order
+                                .setStyle(new NotificationCompat.InboxStyle()
+                                        .addLine("This is the first message")
+                                        .addLine("This is Second message")
+                                        .addLine("This is third message")
+                                        .addLine("This is fourth message")
+                                        .addLine("This is fifth message")
+                                        .addLine("This is sixth message")
+                                        .addLine("This is seventh message")
+                                        .setBigContentTitle("Preview")
+                                        .setSummaryText("Push Notification demo"))
                                 .build();
 
                         notificationManagerCompat.notify(2, notification);
 
-                    }else {
+                    } else {
                         messageET.setError("Message is Empty.");
                     }
-                }else {
+                } else {
                     titleET.setError("Title is Empty.");
                 }
 
